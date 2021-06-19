cf7585eb040a839db61a8737f972e31711e1a2e1
berntpopp <bernt.popp@googlemail.com>
2021-06-19 11:33:34 +0200

    template in about

--- app/src/views/About.vue
+++ app/src/views/About.vue
@@ -1,14 +1,22 @@
 <template>
-  <div style="padding-top: 80px;">
-    <h3>This is an about page</h3>
+  <div class="container-fluid" style="padding-top: 80px;">
+  <b-spinner label="Loading..." v-if="loading" class="float-center m-5"></b-spinner>
+    <b-container fluid v-else>
 
-      <b-card-text class="text-left">
-        The SysNDD database contains gene disease relationships manually curated from the literature based on the previous curation effort in SysID.
-        Our long-term goal is incorporation of the SysNDD/SysID data into other gene disease relationship databases like the Orphanet ontology.
-        To allow interoperability and mapping between gene-, phenotype- or disease-oriented databases we center our approach around curated gene-inheritance-disease units, so called entities, 
-        which are annotated with a predefined list of NDD associated phenotypes.
-      </b-card-text>
+      <b-row class="justify-content-md-center mt-8">
+        <b-col col md="10">
 
+          <h3>About</h3>
+          <b-card-text class="text-left">
+            The SysNDD database contains gene disease relationships manually curated from the literature based on the previous curation effort in SysID.
+            Our long-term goal is incorporation of the SysNDD/SysID data into other gene disease relationship databases like the Orphanet ontology.
+            To allow interoperability and mapping between gene-, phenotype- or disease-oriented databases we center our approach around curated gene-inheritance-disease units, so called entities, 
+            which are annotated with a predefined list of NDD associated phenotypes.
+          </b-card-text>
+
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
