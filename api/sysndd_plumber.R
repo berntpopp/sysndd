657e3e2ec14daf2741e5b732b86f325714a74766
berntpopp <bernt.popp@googlemail.com>
2021-08-15 21:27:21 +0200

    memoise for plot

--- api/sysndd_plumber.R
+++ api/sysndd_plumber.R
@@ -30,6 +30,7 @@ dw <- config::get("sysndd_db_local")
 ##-------------------------------------------------------------------##
 
 
+
 ##-------------------------------------------------------------------##
 pool <- dbPool(
   drv = RMariaDB::MariaDB(),
@@ -43,9 +44,11 @@ pool <- dbPool(
 ##-------------------------------------------------------------------##
 
 
+
 ##-------------------------------------------------------------------##
 # Define functions
 # based on https://xiaolianglin.com/2018/12/05/Use-memoise-to-speed-up-your-R-plumber-API/
+
 nest_gene_tibble <- function(tibble) {
 	nested_tibble <- tibble %>%
 		nest_by(symbol, hgnc_id, category, hpo_mode_of_inheritance_term_name, hpo_mode_of_inheritance_term, .key = "entities")
@@ -53,8 +56,25 @@ nest_gene_tibble <- function(tibble) {
 	return(nested_tibble)
 }
 
-# Memoisize function ------------------------------------------------------
+make_entities_plot <- function(data_tibble) {
+	plot <- ggplot(data = data_tibble , aes(x = entry_date, color = category)) +
+		stat_bin(data=subset(data_tibble, category=="Definitive"), aes(y=cumsum(..count..)), geom="step", bins = 30) +
+		stat_bin(data=subset(data_tibble, category=="Candidate"), aes(y=cumsum(..count..)), geom="step", bins = 30) +
+		theme_classic() +
+		theme(axis.text.x = element_text(angle = -45, hjust = 0), axis.title.x = element_blank(), axis.title.y = element_blank(), legend.position="top", legend.title = element_blank())
+
+	file <- "results/plot.png"
+	ggsave(file, plot, width = 4.5, height = 2.5, dpi = 150, units = "in")
+	return(base64Encode(readBin(file, "raw", n = file.info(file)$size), "txt"))
+}
+##-------------------------------------------------------------------##
+
+
+
+##-------------------------------------------------------------------##
+# Memoisize functions
 nest_gene_tibble_mem <- memoise(nest_gene_tibble)
+make_entities_plot_mem <- memoise(make_entities_plot)
 ##-------------------------------------------------------------------##
 
 
@@ -1334,15 +1354,8 @@ function() {
 		)) %>%
 		filter(ndd_phenotype == "Yes")
 
-	plot <- ggplot(data = sysndd_db_disease_collected , aes(x = entry_date, color = category)) +
-		stat_bin(data=subset(sysndd_db_disease_collected, category=="Definitive"), aes(y=cumsum(..count..)), geom="step", bins = 30) +
-		stat_bin(data=subset(sysndd_db_disease_collected, category=="Candidate"), aes(y=cumsum(..count..)), geom="step", bins = 30) +
-		theme_classic() +
-		theme(axis.text.x = element_text(angle = -45, hjust = 0), axis.title.x = element_blank(), axis.title.y = element_blank(), legend.position="top", legend.title = element_blank())
 
-	file <- "results/plot.png"
-	ggsave(file, plot, width = 4.5, height = 2.5, dpi = 150, units = "in")
-	base64Encode(readBin(file, "raw", n = file.info(file)$size), "txt")
+	make_entities_plot(sysndd_db_disease_collected)
 }
 
 ## Statistics endpoints
@@ -1420,7 +1433,6 @@ function(searchterm, helper = TRUE) {
 	} else {
 		sysndd_db_entity_search_return
 	}
-
 }
 
 ## Search endpoints
