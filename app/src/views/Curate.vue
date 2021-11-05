9a5979cf3dfe4e2049e71d52d55ed423cafb6d82
berntpopp <bernt.popp@googlemail.com>
2021-11-05 13:36:49 +0100

    button style changes

--- app/src/views/Curate.vue
+++ app/src/views/Curate.vue
@@ -1,14 +1,50 @@
 <template>
   <div class="container-fluid" style="min-height:90vh">
-  <b-spinner label="Loading..." v-if="loading" class="float-center m-5"></b-spinner>
-    <b-container fluid v-else>
+    <b-container fluid>
 
       <b-row class="justify-content-md-center py-2">
         <b-col col md="10">
 
-          <h3>Curate</h3>
+          <div>
+          <b-tabs content-class="mt-3" v-model="tabIndex">
+
+            
+            <b-tab title="Entity new" active>
+              <b-spinner label="Loading..." v-if="loadingEntityNew" class="float-center m-5"></b-spinner>
+              <b-container fluid v-else>
+              </b-container>
+            </b-tab>
+
+            <b-tab title="Review approve">
+              <b-spinner label="Loading..." v-if="loadingReviewApprove" class="float-center m-5"></b-spinner>
+              <b-container fluid v-else>
+              </b-container>
+            </b-tab>
+
+            <b-tab title="Status approve">
+              <b-spinner label="Loading..." v-if="loadingStatusApprove" class="float-center m-5"></b-spinner>
+              <b-container fluid v-else>
+              </b-container>
+            </b-tab>
+
+            <b-tab title="Users approve">
+              <b-spinner label="Loading..." v-if="loadingUsersApprove" class="float-center m-5"></b-spinner>
+              <b-container fluid v-else>
+              </b-container>
+            </b-tab>
+
+            <b-tab title="Re-Review curation">
+              <b-spinner label="Loading..." v-if="loadingReReviewCuration" class="float-center m-5"></b-spinner>
+              <b-container fluid v-else>
+              </b-container>
+            </b-tab>
+
+          </b-tabs>
+          </div>
+          
         </b-col>
       </b-row>
+      
     </b-container>
   </div>
 </template>
@@ -17,6 +53,15 @@
 <script>
 export default {
   name: 'Curate',
+    data() {
+      return {
+        loadingEntityNew: true,
+        loadingReviewApprove: true,
+        loadingStatusApprove: true,
+        loadingUsersApprove: true,
+        loadingReReviewCuration: true
+      };
+    }
   }
 </script>
 
