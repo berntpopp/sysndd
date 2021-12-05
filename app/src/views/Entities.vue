eabef8410bedc19278307334881aac7d98db239a
berntpopp <bernt.popp@googlemail.com>
2021-12-05 18:01:39 +0100

    changes to metaInfo

--- app/src/views/Entities.vue
+++ app/src/views/Entities.vue
@@ -232,6 +232,19 @@
 <script>
 export default {
   name: 'Entities',
+  metaInfo: {
+    // if no subcomponents specify a metaInfo.title, this title will be used
+    title: 'Entities',
+    // all titles will be injected into this template
+    titleTemplate: '%s | SysNDD - The expert curated database of gene disease relationships in neurodevelopmental disorders',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'description', content: 'The Entities table view allows browsing NDD associated entities.' }
+    ]
+  },
   data() {
         return {
           stoplights_style: {"Definitive": "success", "Moderate": "primary", "Limited": "warning", "Refuted": "danger"},
