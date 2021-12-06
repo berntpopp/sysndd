5cc4bb24a8ee86771cca68d3c47a7cdd22dfddca
berntpopp <bernt.popp@googlemail.com>
2021-12-06 08:13:08 +0100

    added devtool source-maps generation

--- app/vue.config.js
+++ app/vue.config.js
@@ -1,6 +1,10 @@
 const BootstrapVueLoader = require('bootstrap-vue-loader')
+
+// transpileDependencies and devtool source-map based on https://stackoverflow.com/questions/59693708/how-can-i-activate-the-sourcemap-for-vue-cli-4
 module.exports = {
+    transpileDependencies: [ true ],
     configureWebpack: {
+        devtool: 'source-map',
         plugins: [ new BootstrapVueLoader() ]
     }
 }
