c30963ff06bfeab6c8bf02a9be7c1bd2027ca989
berntpopp <bernt.popp@googlemail.com>
2022-02-13 12:13:47 +0100

    removed transpileDependencies causing comp prob

--- app/vue.config.js
+++ app/vue.config.js
@@ -2,9 +2,7 @@ const BootstrapVueLoader = require('bootstrap-vue-loader')
 
 // transpileDependencies and devtool source-map based on https://stackoverflow.com/questions/59693708/how-can-i-activate-the-sourcemap-for-vue-cli-4
 module.exports = {
-    transpileDependencies: [ true ],
     configureWebpack: {
-        devtool: 'source-map',
         plugins: [ new BootstrapVueLoader() ]
     },
     css: {
