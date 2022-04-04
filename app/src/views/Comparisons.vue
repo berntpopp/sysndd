e0ba782a0a842cddd299f6ce82dcb20f6b740700
berntpopp <bernt.popp@googlemail.com>
2022-04-04 16:41:30 +0200

    added server side pagination to comparision view

--- app/src/views/Comparisons.vue
+++ app/src/views/Comparisons.vue
@@ -40,10 +40,12 @@
                 size="sm">
                   <b-form-input
                     id="filter-input"
-                    v-model="filter"
+                    v-model="filter['any']"
                     type="search"
                     placeholder="any field by typing here"
                     debounce="500"
+                    @click="removeFilters()"
+                    @update="filtered()"
                   >
                   </b-form-input>
                 </b-input-group>
@@ -71,13 +73,14 @@
               </b-input-group>
 
               <b-pagination
+                @change="handlePageChange"
                 v-model="currentPage"
                 :total-rows="totalRows"
                 :per-page="perPage"
                 align="fill"
                 size="sm"
                 class="my-0"
-                last-number
+                limit=2
               ></b-pagination>
             </b-col>
           </b-row>
@@ -88,13 +91,12 @@
               <b-spinner label="Loading..." v-if="loadingTable" class="float-center m-5"></b-spinner>
               <b-table
                 :items="items"
+                :fields="fields"
                 :current-page="currentPage"
-                :per-page="perPage"
-                :filter="filter"
                 :filter-included-fields="filterOn"
                 :sort-by.sync="sortBy"
                 :sort-desc.sync="sortDesc"
-                :sort-direction="sortDirection"
+                :busy="isBusy"
                 stacked="md"
                 head-variant="light"
                 show-empty
@@ -103,8 +105,8 @@
                 striped
                 hover
                 sort-icon-left
-                v-else
-                @filtered="onFiltered"
+                no-local-sorting
+                no-local-pagination
               >
 
               <template #cell(symbol)="data">
@@ -277,30 +279,110 @@
         width: 1200,
         height: 600,
         items: [],
+        fields: [
+          { 
+            key: 'symbol', 
+            label: 'Symbol', 
+            sortable: true, 
+            filterable: true, 
+            class: 'text-left' 
+          },
+          { 
+            key: 'SysNDD', 
+            label: 'SysNDD', 
+            sortable: true, 
+            filterable: true, 
+            class: 'text-left' 
+          },
+          { 
+            key: 'radboudumc_ID', 
+            label: 'Radboud UMC ID', 
+            sortable: true, 
+            filterable: true, 
+            class: 'text-left' 
+          },
+          { 
+            key: 'gene2phenotype', 
+            label: 'gene2phenotype', 
+            sortable: true, 
+            filterable: true, 
+            class: 'text-left' 
+          },
+          { 
+            key: 'panelapp', 
+            label: 'PanelApp', 
+            sortable: true, 
+            filterable: true, 
+            class: 'text-left' 
+          },
+          { 
+            key: 'sfari', 
+            label: 'SFARI', 
+            sortable: true, 
+            filterable: true, 
+            class: 'text-left' 
+          },
+          { 
+            key: 'geisinger_DBD', 
+            label: 'Geisinger DBD', 
+            sortable: true, 
+            filterable: true, 
+            class: 'text-left' 
+          },
+          { 
+            key: 'omim_ndd', 
+            label: 'OMIM NDD', 
+            sortable: true, 
+            filterable: true, 
+            class: 'text-left' 
+          },
+          { 
+            key: 'orphanet_id', 
+            label: 'Orphanet ID', 
+            sortable: true, 
+            filterable: true, 
+            class: 'text-left' 
+          },
+        ],
         totalRows: 0,
         currentPage: 1,
+        currentItemID: 0,
+        prevItemID: null,
+        nextItemID: null,
+        lastItemID: null,
+        executionTime: 0,
         perPage: 10,
         pageOptions: [10, 25, 50, { value: 100, text: "Show a lot" }],
-        sortBy: '',
+        sortBy: 'symbol',
         sortDesc: false,
-        sortDirection: 'asc',
-        filter: null,
+        filter: {any: ''}, 
         filterOn: [],
         selection: null,
         image: '',
         loadingUpset: true,
         loadingMatrix: true,
         loadingTable: true,
-        tabIndex: 0
+        tabIndex: 0,
+        isBusy: true
       };
     },
     watch: {
       tabIndex(value) {
         if (value === 2 & this.loadingTable) {
           this.loadTableData();
+          this.loadingTable = false;
         } else if (value === 1 & this.loadingMatrix) {
           this.loadMatrixPlot();
         }
+      },
+      sortBy(value) {
+        this.handleSortChange();
+      },
+      perPage(value) {
+        this.handlePerPageChange();
+      },
+      sortDesc(value) {
+        this.handleSortChange();
       }
     },
     computed: {
@@ -312,6 +394,48 @@
       this.loadComparisonsUpsetData();
     },
     methods: {
+        handleSortChange() {
+          this.currentItemID = 0;
+          this.loadTableData();
+        },
+        handlePerPageChange() {
+          this.currentItemID = 0;
+          this.loadTableData();
+        },
+        handlePageChange(value) {
+          if (value == 1) {
+            this.currentItemID = 0;
+            this.loadTableData();
+          } else if (value == this.totalPages) {
+            this.currentItemID = this.lastItemID;
+            this.loadTableData();
+          } else if (value > this.currentPage) {
+            this.currentItemID = this.nextItemID;
+            this.loadTableData();
+          } else if (value < this.currentPage) {
+            this.currentItemID = this.prevItemID;
+            this.loadTableData();
+          }
+        },
+        filtered() {
+          let filter_string_not_empty = Object.filter(this.filter, value => value !== '');
+
+          if (Object.keys(filter_string_not_empty).length !== 0) {
+            this.filter_string = 'contains(' + Object.keys(filter_string_not_empty).map((key) => [key, this.filter[key]].join(',')).join('),contains(') + ')';
+            this.loadTableData();
+          } else {
+            this.filter_string = '';
+            this.loadTableData();
+          }
+        },
+        removeFilters() {
+          this.filter = {any: '', entity_id: '', symbol: '', disease_ontology_name: '', disease_ontology_id_version: '', hpo_mode_of_inheritance_term_name: '', hpo_mode_of_inheritance_term: '', ndd_phenotype: '', category: ''};
+          this.filtered();
+        },
+        removeSearch() {
+          this.filter['any']  = '';
+          this.filtered();
+        },
         async loadComparisonsUpsetData() {
           this.loadingUpset = true;
           let apiUrl = process.env.VUE_APP_API_URL + '/api/comparisons/upset';
@@ -326,16 +450,27 @@
 
         },
         async loadTableData() {
-          this.loadingTable = true;
-          let apiUrl = process.env.VUE_APP_API_URL + '/api/comparisons/table';
+          this.isBusy = true;
+          let apiUrl = process.env.VUE_APP_API_URL + '/api/comparisons/table?sort=' + ((this.sortDesc) ? '-' : '+') + this.sortBy + '&filter=' + this.filter_string + '&page[after]=' + this.currentItemID + '&page[size]=' + this.perPage;
+          
           try {
             let response = await this.axios.get(apiUrl);
-            this.items = response.data;
-            this.totalRows = response.data.length;
+            this.items = response.data.data;
+
+            this.totalRows = response.data.meta[0].totalItems;
+            this.currentPage = response.data.meta[0].currentPage;
+            this.totalPages = response.data.meta[0].totalPages;
+            this.prevItemID = response.data.meta[0].prevItemID;
+            this.currentItemID = response.data.meta[0].currentItemID;
+            this.nextItemID = response.data.meta[0].nextItemID;
+            this.lastItemID = response.data.meta[0].lastItemID;
+            this.executionTime = response.data.meta[0].executionTime;
+
+            this.isBusy = false;
+
           } catch (e) {
             console.error(e);
           }
-          this.loadingTable = false;
         },
         async loadMatrixPlot() {
           this.loadingMatrix = true;
