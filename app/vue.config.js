0ebd56c975200bf335736336d1f3f6611c7dde64
berntpopp <bernt.popp@googlemail.com>
2022-04-22 20:02:04 +0200

    disabled prefetch

--- app/vue.config.js
+++ app/vue.config.js
@@ -1,9 +1,14 @@
 const BootstrapVueLoader = require('bootstrap-vue-loader')
 
 // transpileDependencies and devtool source-map based on https://stackoverflow.com/questions/59693708/how-can-i-activate-the-sourcemap-for-vue-cli-4
+// prefetch delete based on https://github.com/vuejs/vue-cli/issues/979
 module.exports = {
     configureWebpack: {
         plugins: [ new BootstrapVueLoader() ]
+    },
+    chainWebpack: (config) => {
+      // A, remove the plugin
+      config.plugins.delete('prefetch')
     },
     css: {
         loaderOptions: {
@@ -11,5 +16,5 @@ module.exports = {
             prependData: `@import "@/assets/scss/custom.scss";`
           }
         }
-      }
+      },
 }
