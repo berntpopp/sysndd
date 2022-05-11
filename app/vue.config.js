06e0dcefc29ce3c1a641860e2c6ea405fb884aa2
berntpopp <bernt.popp@googlemail.com>
2022-05-11 09:38:13 +0200

    remove sitemap-webpack-plugin

--- app/vue.config.js
+++ app/vue.config.js
@@ -1,72 +1,11 @@
 const BootstrapVueLoader = require('bootstrap-vue-loader')
-const SitemapPlugin = require('sitemap-webpack-plugin').default;
-
-// object paths
-
-const paths = [
-  {
-    path: '/',
-    priority: 1.0,
-    changefreq: 'monthly'
-  },
-  {
-    path: '/Entities',
-    priority: 0.9,
-    changefreq: 'monthly'
-  },
-  {
-    path: '/Genes',
-    priority: 0.9,
-    changefreq: 'monthly'
-  },
-  {
-    path: '/Phenotypes',
-    priority: 0.9,
-    changefreq: 'monthly'
-  },
-  {
-    path: '/CurationComparisons',
-    priority: 0.8,
-    changefreq: 'monthly'
-  },
-  {
-    path: '/PhenotypeCorrelations',
-    priority: 0.8,
-    changefreq: 'monthly'
-  },
-  {
-    path: '/EntriesOverTime',
-    priority: 0.8,
-    changefreq: 'monthly'
-  },
-  {
-    path: '/GeneNetworks',
-    priority: 0.8,
-    changefreq: 'monthly'
-  },
-  {
-    path: '/About',
-    priority: 0.5,
-    changefreq: 'yearly'
-  },
-  {
-    path: '/Login',
-    priority: 0.5,
-    changefreq: 'yearly'
-  },
-  {
-    path: '/Register',
-    priority: 0.5,
-    changefreq: 'yearly'
-  }
-]
 
 // transpileDependencies and devtool source-map based on https://stackoverflow.com/questions/59693708/how-can-i-activate-the-sourcemap-for-vue-cli-4
 // prefetch delete based on https://github.com/vuejs/vue-cli/issues/979
 // module in webpack fpr pinia import based on: https://github.com/vuejs/pinia/issues/675
 module.exports = {
     configureWebpack: {
-        plugins: [ new BootstrapVueLoader(), new SitemapPlugin({ base: 'https://sysndd.dbmr.unibe.ch/', paths, options: {lastmod: true} }) ],
+        plugins: [ new BootstrapVueLoader(), ],
         module: {
           rules: [
             {
