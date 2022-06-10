11f53a4988db790d98d320868e5136276803e3a9
berntpopp <bernt.popp@googlemail.com>
2022-06-10 10:33:55 +0200

    change box size

--- app/src/components/analyses/AnalyseGeneClusters.vue
+++ app/src/components/analyses/AnalyseGeneClusters.vue
@@ -60,7 +60,7 @@
       generateClusterGraph() {
       // Graph dimension
       const margin = {top: 50, right: 50, bottom: 50, left: 50},
-          width = 800 - margin.left - margin.right,
+          width = 600 - margin.left - margin.right,
           height = 600 - margin.top - margin.bottom;
 
       // Create the svg area
