cfdbadfb91b4406a27ef65033e3a6afed97f9dea
berntpopp <bernt.popp@googlemail.com>
2022-06-13 07:58:41 +0200

    add libs to reduce css footrprint

--- app/vue.config.js
+++ app/vue.config.js
@@ -6,7 +6,7 @@ const { routes } = require('./src/router/routes.js');
 
 // transpileDependencies and devtool source-map based on https://stackoverflow.com/questions/59693708/how-can-i-activate-the-sourcemap-for-vue-cli-4
 // prefetch delete based on https://github.com/vuejs/vue-cli/issues/979
-// module in webpack fpr pinia import based on: https://github.com/vuejs/pinia/issues/675
+// module in webpack for pinia import based on: https://github.com/vuejs/pinia/issues/675
 module.exports = {
     configureWebpack: {
         plugins: [ new BootstrapVueLoader(), ],
