d03fa2876e62052b584fea5537c23adb29e39ba6
berntpopp <bernt.popp@googlemail.com>
2022-06-26 21:57:49 +0200

    add logging and fix exit hook

---
+++ api/logs/README.md
@@ -0,0 +1,3 @@
+# SysNDD-API logs folder
+
+This folder holds log files from the API.
--- api/start_sysndd_api.R
+++ api/start_sysndd_api.R
@@ -1,8 +1,49 @@
+##-------------------------------------------------------------------##
+# load libraries
 library(plumber)
+library(logger)
+library(tictoc)
+##-------------------------------------------------------------------##
 
+
+##-------------------------------------------------------------------##
+# set work directory
 setwd("/sysndd_api_volume")
+##-------------------------------------------------------------------##
 
+##-------------------------------------------------------------------##
+# Specify how logs are written
+log_dir <- "logs"
+if (!fs::dir_exists(log_dir)) fs::dir_create(log_dir)
+log_appender(appender_file(tempfile("plumber_", log_dir, ".log")))
+
+# helper function to handle empty string
+convert_empty <- function(string) {
+  if (string == "") {
+    "-"
+  } else {
+    string
+  }
+}
+##-------------------------------------------------------------------##
+
+
+##-------------------------------------------------------------------##
+# start the API
 root <- pr("sysndd_plumber.R") %>%
+    pr_hook("preroute", function() { tictoc::tic() }) %>%
+    pr_hook("postroute", function(req, res) { 
+            end <- tictoc::toc(quiet = TRUE)
+             # Log details about the request and the response
+            log_info(paste0("{convert_empty(req$REMOTE_ADDR)} ",
+            "'{convert_empty(req$HTTP_USER_AGENT)}' ",
+            "{convert_empty(req$HTTP_HOST)} ",
+            "{convert_empty(req$REQUEST_METHOD)} ",
+            "{convert_empty(req$PATH_INFO)} ",
+            "{convert_empty(res$status)} ",
+            "{round(end$toc - end$tic, digits = getOption('digits', 5))}")
+            )
+        }) %>%
     pr_set_api_spec(function(spec) {
       spec$components$securitySchemes$bearerAuth$type <- "http"
       spec$components$securitySchemes$bearerAuth$scheme <- "bearer"
@@ -10,7 +51,4 @@ root <- pr("sysndd_plumber.R") %>%
       spec$security[[1]]$bearerAuth <- ""
       spec
     }) %>%
-        pr_run(host = "0.0.0.0", port = 7777) %>%
-    pr_hook("exit", function() {
-        poolClose(pool)
-      })
+        pr_run(host = "0.0.0.0", port = 7778)
