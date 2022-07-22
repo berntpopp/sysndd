23a84692ff403e61bdc593ec2e2f91fb4f71459e
berntpopp <bernt.popp@googlemail.com>
2022-07-22 20:26:50 +0200

    remove importWorkboxFrom from pwa workboxOptions

--- app/vue.config.js
+++ app/vue.config.js
@@ -35,7 +35,6 @@ module.exports = {
     name: 'SysNDD',
     themeColor: '#EAADBA',
     workboxOptions: {
-      importWorkboxFrom: 'local',
       // added acording to https://stackoverflow.com/questions/54145735/vue-pwa-not-getting-new-content-after-refresh
       skipWaiting: true,
     },
