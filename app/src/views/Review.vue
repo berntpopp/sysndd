c3fbcd733530a12dd982710f5f6f4c733c0c2604
berntpopp <bernt.popp@googlemail.com>
2022-10-18 19:44:56 +0200

    fix separator for GeneReviews

--- app/src/views/Review.vue
+++ app/src/views/Review.vue
@@ -612,7 +612,7 @@
               input-id="review-genereviews-select"
               no-outer-focus
               class="my-0"
-              separator=" ,;"
+              separator=",;"
               :tag-validator="tagValidatorPMID"
               remove-on-delete
             >
