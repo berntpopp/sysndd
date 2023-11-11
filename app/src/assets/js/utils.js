664781c7a4d25b93e0604440ef8e581be16e584d
Bernt Popp <bernt.popp@gmail.com>
2023-11-11 10:37:43 +0100

    update utilities.js

--- app/src/assets/js/utils.js
+++ app/src/assets/js/utils.js
@@ -1,8 +1,17 @@
-// utils.js
+/**
+ * @fileoverview Utility functions for common operations.
+ */
+
 export default {
-  // Function to truncate a string to a specified length.
-  // If the string is longer than the specified length, it adds '...' to the end.
+  /**
+   * Truncates a string to a specified length including the ellipsis.
+   * If the string is longer than the specified length, it adds '...' to the end.
+   *
+   * @param {string} str - The string to truncate.
+   * @param {number} n - The maximum length of the string including the ellipsis.
+   * @returns {string} The truncated string with ellipsis if it exceeds the specified length.
+   */
   truncate(str, n) {
-    return str.length > n ? `${str.substr(0, n - 1)}...` : str;
+    return str.length > n ? `${str.substr(0, n - 3)}...` : str;
   },
 };
