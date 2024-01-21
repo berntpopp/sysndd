23a8495f5db565167602f0abb9e1bbf240d64caa
Bernt Popp <bernt.popp@gmail.com>
2024-01-21 18:07:01 +0100

    feat(ontology-functions): Enhance ontology processing and data handling
    - Add `identify_critical_ontology_changes` function to determine significant changes between updated and current ontology sets, focusing on entities' usage.
    - Implement `process_combine_ontology` to merge and process MONDO and OMIM ontology data, with checks for existing data files based on their age.
    - Update `get_mondo_mappings` to return a tibble of MONDO ontology mappings with selectable columns.
    - Enhance `process_mondo_ontology` for streamlined processing of MONDO ontology data from a specified file.
    - Refine `process_omim_ontology` to use HGNC gene list and mode of inheritance list for processing OMIM ontology data.
    - Modify `get_ontology_object` to support downloading and loading HPO, MPO, and MONDO ontologies with configurable parameters for file age and tag types.
    - Introduce logic checks and summarization improvements in the processing of ontology mappings to retain NA values effectively.
    - Ensure efficient handling of ontology data files by checking their age before loading or regenerating them.

--- api/functions/ontology-functions.R
+++ api/functions/ontology-functions.R
@@ -3,6 +3,155 @@ require(ontologyIndex) # Needed to read ontology files
 require(tidyverse)     # For data manipulation
 
 
+#' Identify Critical Ontology Changes
+#'
+#' This function identifies critical changes in ontology sets by comparing the updated
+#' ontology set with the current ontology set and a subset of terms used in entities.
+#' It returns a tibble of critical changes.
+#'
+#' @param disease_ontology_set_update Updated disease ontology dataset.
+#' @param disease_ontology_set Current disease ontology dataset.
+#' @param ndd_entity_view_ontology_set Ontology dataset used in entities.
+#' @return A tibble of critical ontology changes.
+#'
+#' @examples
+#' \dontrun{
+#'   critical_changes <- identify_critical_ontology_changes(
+#'     disease_ontology_set_update,
+#'     disease_ontology_set,
+#'     ndd_entity_view_ontology_set
+#'   )
+#' }
+#'
+#' @export
+identify_critical_ontology_changes <- function(disease_ontology_set_update, disease_ontology_set, ndd_entity_view_ontology_set) {
+  # Add columns for logic checks in the updated set
+  disease_ontology_set_update_extra <- disease_ontology_set_update %>%
+    select(disease_ontology_id_version, disease_ontology_id, hgnc_id, hpo_mode_of_inheritance_term, disease_ontology_name) %>%
+    mutate(id_hgnc_hpo = paste0(disease_ontology_id, "_", hgnc_id, "_", hpo_mode_of_inheritance_term),
+           name_hgnc_hpo = paste0(disease_ontology_name, "_", hgnc_id, "_", hpo_mode_of_inheritance_term),
+           has_id_version = str_detect(disease_ontology_id_version, "_")) %>%
+    group_by(disease_ontology_id) %>%
+    mutate(id_same_name = length(unique(disease_ontology_name))) %>%
+    ungroup()
+
+  # Add columns for logic checks in the current set
+  disease_ontology_set_current <- disease_ontology_set %>%
+    mutate(id_hgnc_hpo = paste0(disease_ontology_id, "_", hgnc_id, "_", hpo_mode_of_inheritance_term),
+           name_hgnc_hpo = paste0(disease_ontology_name, "_", hgnc_id, "_", hpo_mode_of_inheritance_term),
+           has_id_version = str_detect(disease_ontology_id_version, "_")) %>%
+    group_by(disease_ontology_id) %>%
+    mutate(id_same_name = length(unique(disease_ontology_name))) %>%
+    ungroup()
+
+  # Filter current ontology list for terms used in entities
+  disease_ontology_set_current_used <- disease_ontology_set_current %>%
+    mutate(used_in_entity = disease_ontology_id_version %in% ndd_entity_view_ontology_set$disease_ontology_id_version) %>%
+    filter(used_in_entity) %>%
+    select(-used_in_entity)
+
+  # Generate a tibble of potentially problematic entities and filter these
+  # TODO: define columns for check function to understand logic
+  disease_ontology_set_current_used_check <- disease_ontology_set_current_used %>%
+    mutate(check_ontology_id_version = disease_ontology_id_version %in% disease_ontology_set_update_extra$disease_ontology_id_version,
+           check_ontology_name = disease_ontology_name %in% disease_ontology_set_update_extra$disease_ontology_name,
+           check_id_fingerprint = id_hgnc_hpo %in% disease_ontology_set_update_extra$id_hgnc_hpo,
+           check_name_fingerprint = name_hgnc_hpo %in% disease_ontology_set_update_extra$name_hgnc_hpo)
+
+  disease_ontology_set_current_used_check_filter <- disease_ontology_set_current_used_check %>%
+    filter(!check_ontology_id_version | !check_ontology_name)
+
+  # Compute if a mismatch is critical or not
+  critical <- disease_ontology_set_current_used_check_filter %>%
+    mutate(critical = case_when(
+      !has_id_version & check_ontology_id_version & !check_ontology_name ~ FALSE,
+      has_id_version & check_ontology_id_version & !check_ontology_name & id_same_name == 1 ~ FALSE,
+      TRUE ~ TRUE
+    ),
+    automatic_assignment_version = !check_ontology_id_version & !check_ontology_name & check_id_fingerprint,
+    automatic_assignment_name = !check_ontology_id_version & check_ontology_name) %>%
+    filter(critical)
+
+  return(critical)
+}
+
+
+#' Process and Combine Ontology Data
+#'
+#' This function processes MONDO and OMIM ontology data, combines them, and 
+#' returns the combined dataset. If a recent CSV file exists, it loads that; 
+#' otherwise, it regenerates the data and saves it as a CSV file.
+#'
+#' @param mode_of_inheritance_list A tibble containing mode of inheritance data.
+#' @param non_alt_loci_set A tibble containing non-alternative loci gene data.
+#' @param max_file_age Integer, maximum age of the file in months before regeneration.
+#' @param output_path String, the path where the output CSV file will be stored.
+#' @return A tibble containing the combined ontology dataset.
+#'
+#' @examples
+#' \dontrun{
+#'   moi_list <- read_csv("path/to/moi_list.csv")
+#'   non_alt_loci_set <- read_csv("path/to/non_alt_loci_set.csv")
+#'   combined_ontology_data <- process_combine_ontology(moi_list, non_alt_loci_set, 3, "data/")
+#' }
+#'
+#' @export
+process_combine_ontology <- function(hgnc_list, mode_of_inheritance_list, max_file_age = 3, output_path = "data/") {
+  csv_file_name <- paste0(output_path, "disease_ontology_set.", format(Sys.Date(), "%Y-%m-%d"), ".csv")
+
+  # Check if file exists and is not too old
+  if (check_file_age("disease_ontology_set", output_path, 1)) {
+    return(read_csv(get_newest_file("disease_ontology_set", output_path), na = "NULL")) # Load and return the existing tibble
+  } else {
+    # Process ontology if file is too old or doesn't exist
+    mondo_terms <- process_mondo_ontology()
+    omim_terms <- process_omim_ontology(hgnc_list, mode_of_inheritance_list)
+
+    # Get ontology mappings
+    config_vars <- list(
+      hpo_obo_url = "https://github.com/obophenotype/human-phenotype-ontology/releases/download/v2023-10-09/hp.obo",
+      mpo_obo_url = "https://github.com/mgijax/mammalian-phenotype-ontology/releases/download/v2023-10-31/mp.obo",
+      mondo_obo_url = "http://purl.obolibrary.org/obo/mondo.obo",
+      download_path = "data/"
+    )
+    mondo_ontology <- get_ontology_object("mondo", config_vars)
+    mondo_mappings <- get_mondo_mappings(mondo_ontology)
+
+    # make mappings for specific joins
+    # this splits columns with multiple values (separated by ";") into multiple rows for OMIM
+    # then it groups by OMIM summarizes the other columns (";" separated) ommiting NA values
+    mondo_mappings_omim <- mondo_mappings %>%
+      filter(OMIM != "NA") %>%
+      separate_rows(OMIM, sep = ";") %>%
+      unique() %>%
+      group_by(OMIM) %>%
+      summarize(
+        MONDO = ifelse(all(is.na(MONDO)), NA, paste0(na.omit(unique(MONDO)), collapse = ";")),
+        DOID = ifelse(all(is.na(DOID)), NA, paste0(na.omit(unique(DOID)), collapse = ";")),
+        Orphanet = ifelse(all(is.na(Orphanet)), NA, paste0(na.omit(unique(Orphanet)), collapse = ";")),
+        EFO = ifelse(all(is.na(EFO)), NA, paste0(na.omit(unique(EFO)), collapse = ";")),
+        .groups = 'drop'
+      )
+
+    # Combine data
+    # TODO: for the mondo terms copy their values to the modo column
+    omim_terms_mappings <- left_join(omim_terms, mondo_mappings_omim, by = c("disease_ontology_id_version" = "OMIM"))
+
+    mondo_terms_mappings <- left_join(mondo_terms, mondo_mappings, by = c("disease_ontology_id_version" = "MONDO"))
+
+    disease_ontology_set <- bind_rows(mondo_terms_mappings, omim_terms_mappings) %>%
+      select(-OMIM) %>%
+      mutate(is_active = TRUE, update_date = format(Sys.Date(), "%Y-%m-%d"))
+
+    # Save the result as a CSV file
+    write_csv(disease_ontology_set, file = csv_file_name, na = "NULL")
+
+    # Return the tibble
+    return(disease_ontology_set)
+  }
+}
+
+
 #' Process MONDO ontology and save the mapping as a CSV file, then return the mapping tibble.
 #'
 #' This function processes the MONDO ontology to get all descendant terms and their mappings.
@@ -22,12 +171,12 @@ require(tidyverse)     # For data manipulation
 #' }
 #'
 #' @export
-get_mondo_mappings <- function(mondo_ontology, max_age, output_path, columns_to_return = c("OMIM", "MONDO", "DOID", "Orphanet")) {
-  csv_file_name <- paste0(output_path, "mondo_ontology_mapping_", format(Sys.Date(), "%Y-%m-%d"), ".csv")
+get_mondo_mappings <- function(mondo_ontology, max_age = 1, output_path = "data/", columns_to_return = c("OMIM", "MONDO", "DOID", "Orphanet", "EFO")) {
+  csv_file_name <- paste0(output_path, "mondo_ontology_mapping.", format(Sys.Date(), "%Y-%m-%d"), ".csv")
 
   # Check if file exists and is not too old
-  if (file.exists(csv_file_name) && !check_file_age(csv_file_name, max_age)) {
-    mappings_tibble <- read_csv(csv_file_name, na = "NULL") # Load the existing tibble
+  if (check_file_age("mondo_ontology_mapping", output_path, 1)) {
+    mappings_tibble <- read_csv(get_newest_file("mondo_ontology_mapping", output_path), na = "NULL") # Load the existing tibble
   } else {
     # Process ontology if file is too old or doesn't exist
     all_terms <- get_descendants(ontology = mondo_ontology, roots = "MONDO:0000001")
@@ -57,12 +206,6 @@ get_mondo_mappings <- function(mondo_ontology, max_age, output_path, columns_to_
   }
 }
 
-# Helper function to check the age of a file
-check_file_age <- function(file_path, max_age_months) {
-  file_age_days <- as.numeric(difftime(Sys.Date(), file.mtime(file_path), units = "days"))
-  return(file_age_days > (max_age_months * 30))
-}
-
 
 #' Process MONDO Ontology Data
 #'
@@ -80,10 +223,13 @@ check_file_age <- function(file_path, max_age_months) {
 #' @export
 process_mondo_ontology <- function(mondo_file = "data/mondo_terms/mondo_terms.txt") {
 
+  # TODO: replace with function
+  mondo_file_date <- strftime(as.POSIXlt(Sys.time(), "UTC", "%Y-%m-%dT%H:%M:%S"), "%Y-%m-%d")
+
   # TODO: use the get_ontology_object fucntion and a list of mondo identifiers to compute this table
   mondo_terms <- read_delim(mondo_file, "\t", col_names = TRUE) %>%
     mutate(disease_ontology_source = "mondo") %>%
-    mutate(disease_ontology_date = omim_file_date) %>%
+    mutate(disease_ontology_date = mondo_file_date) %>%
     mutate(disease_ontology_is_specific = FALSE) %>%
     mutate(hgnc_id = NA) %>%
     mutate(hpo_mode_of_inheritance_term = NA) %>%
@@ -114,12 +260,14 @@ process_omim_ontology <- function(hgnc_list, moi_list, max_file_age = 3) {
   # TODO: add an initial check to see if the result file already exists and is not too old, return this file if it exists
   # TODO: add a flag to recalculate the file even if it exists
 
+  # TODO: replace with function
+  omim_file_date <- strftime(as.POSIXlt(Sys.time(), "UTC", "%Y-%m-%dT%H:%M:%S"), "%Y-%m-%d")
+
   # Download and load OMIM genemap2 file
   if (check_file_age("genemap2", "data", max_file_age)) {
     genemap2 <- read_delim(get_newest_file("genemap2", "data"), "\t", escape_double = FALSE, col_names = FALSE, comment = "#", trim_ws = TRUE, show_col_types = FALSE) %>%
     select(Chromosome = X1,  Genomic_Position_Start = X2, Genomic_Position_End = X3, Cyto_Location = X4, Computed_Cyto_Location = X5, MIM_Number = X6, Gene_Symbols = X7, Gene_Name = X8, Approved_Symbol = X9, Entrez_Gene_ID = X10, Ensembl_Gene_ID = X11, Comments = X12, Phenotypes = X13, Mouse_Gene_Symbol_ID = X14)
   } else {
-    omim_file_date <- strftime(as.POSIXlt(Sys.time(), "UTC", "%Y-%m-%dT%H:%M:%S"), "%Y-%m-%d")
 
     genemap2_link <- as_tibble(read_lines("data/omim_links/omim_links.txt")) %>%
       mutate(file_name = str_remove(value, "https.+\\/")) %>%
