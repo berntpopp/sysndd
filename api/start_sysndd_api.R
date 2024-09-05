3657e86224b421dcc0c73d6b58b41f01b8d833b3
Bernt Popp <bernt.popp@gmail.com>
2024-09-05 12:20:09 +0200

    fix: Update logging in start_sysndd_api.R

--- api/start_sysndd_api.R
+++ api/start_sysndd_api.R
@@ -35,7 +35,8 @@ dw <- config::get(Sys.getenv("API_CONFIG"))
 # Specify how logs are written
 log_dir <- "logs"
 if (!fs::dir_exists(log_dir)) fs::dir_create(log_dir)
-log_appender(appender_file(tempfile("plumber_", log_dir, ".log")))
+logging_temp_file <- tempfile("plumber_", log_dir, ".log")
+log_appender(appender_file(logging_temp_file))
 ##-------------------------------------------------------------------##
 
 ##-------------------------------------------------------------------##
@@ -61,10 +62,22 @@ root <- pr("sysndd_plumber.R") %>%
       collapse = ""
     )
 
-    log_info(log_entry)
+    log_info(skip_formatter(log_entry))
 
     # Write log entry to database
-    log_message_to_db(log_entry, "INFO")
+    log_message_to_db(
+      address = convert_empty(req$REMOTE_ADDR),
+      agent = convert_empty(req$HTTP_USER_AGENT),
+      host = convert_empty(req$HTTP_HOST),
+      request_method = convert_empty(req$REQUEST_METHOD),
+      path = convert_empty(req$PATH_INFO),
+      query = convert_empty(req$QUERY_STRING),
+      post = convert_empty(req$postBody),
+      status = convert_empty(res$status),
+      duration = round(end$toc - end$tic, digits = getOption("digits", 5)),
+      file = logging_temp_file,
+      modified = Sys.time()
+    )
   }) %>%
   pr_set_api_spec(function(spec) {
     spec$components$securitySchemes$bearerAuth$type <- "http"
